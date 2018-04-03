f53789caa0751c35e692a8275fb0c972841d5082
Patrick Batey <pbatey@patrickBatey-macBook.local>
2018-04-03 14:23:50 -0400

    HTML & Document structure

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	init();
+});
+
+function init() {
+	console.log('init');
+	var t = TweenMax;
+	var tl = new TimelineMax({});
+
+	var arrowLeft = $('.arrowLeft');
+	var arrowRight = $('.arrowRight');
+
+	$(arrowLeft).click(function(){
+
+	});
+
+	$(arrowRight).click(function(){
+
+	});
+
+
+}
